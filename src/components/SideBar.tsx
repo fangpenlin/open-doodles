fc31741cc58b8fdc4fcc055e01a1549db7609dcd
Fang-Pen Lin <hello@fangpenlin.com>
2019-09-01 19:00:19 -0700

    Refactor css

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,5 @@
+import './SideBar.css';
+
 import { default as DoodleProps } from './doodles/Props';
 import React from 'react';
 
@@ -27,15 +29,19 @@ const SideBar: React.FC<Props> = (props) => {
 					return (
 						<div
 							key={index}
-							className="div-block-2"
+							className="SideBar-option-block"
 							style={{ ...index === selectedIndex ? { borderColor: 'black' } : {} }}
 							onClick={() => onSelect(index)}
 						>
-							<div className="div-block" style={{ backgroundColor: config.inkColor }} />
-							<div className="div-block" style={{ backgroundColor: config.accentColor }} />
+							<div className="SideBar-option" style={{ backgroundColor: config.inkColor }} />
+							<div className="SideBar-option" style={{ backgroundColor: config.accentColor }} />
 							<div
 								className={
-									config.backgroundColor == '#FFFFFF00' ? 'div-block checkerboard-bg' : 'div-block'
+									config.backgroundColor == '#FFFFFF00' ? (
+										'SideBar-option checkerboard-bg'
+									) : (
+										'SideBar-option'
+									)
 								}
 								style={{ backgroundColor: config.backgroundColor }}
 							/>
@@ -43,11 +49,40 @@ const SideBar: React.FC<Props> = (props) => {
 					);
 				})}
 			</div>
-			<a href="#" className="button w-button" onClick={onDownloadPack}>
+			<a href="#" className="SideBar-button SideBar-w-button" onClick={onDownloadPack}>
 				Download Pack
 			</a>
-			<div className="div-block-3">
-				<a href="#">Studio File</a>
+			<div className="SideBar-source-block">
+				<a href="#">
+					<svg
+						width="22px"
+						height="22px"
+						viewBox="0 0 22 22"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<g id="Web-App" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+							<g id="Desktop-HD" transform="translate(-24.000000, -550.000000)">
+								<g id="Source-FIles" transform="translate(24.000000, 549.000000)">
+									<g id="Group" transform="translate(0.000000, 1.000000)">
+										<g id="Studio-Logo">
+											<rect id="Rectangle" fill="#FF4183" x="0" y="0" width="22" height="22" />
+											<rect
+												id="Rectangle"
+												fill="#FFFFFF"
+												x="12.1"
+												y="12.1"
+												width="5.5"
+												height="5.5"
+											/>
+										</g>
+									</g>
+								</g>
+							</g>
+						</g>
+					</svg>
+					Studio
+				</a>
 				<a href="#">Sketch File</a>
 			</div>
 		</div>
