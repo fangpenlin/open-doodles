8d58893b5bbcc11f42c9e658046209552480e28b
Fang-Pen Lin <hello@fangpenlin.com>
2019-09-14 13:57:29 -0700

    Move studio logo out as a component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,7 @@
 import "./SideBar.css";
 
 import React from "react";
+import StudioLogo from "./icons/StudioLogo";
 
 export interface ColorConfig {
   readonly inkColor: string;
@@ -65,50 +66,7 @@ const SideBar: React.FC<Props> = props => {
       </button>
       <div className="SideBar-source-block">
         <a href="#">
-          <svg
-            className="logo"
-            width="22px"
-            height="22px"
-            viewBox="0 0 22 22"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g
-              id="Web-App"
-              stroke="none"
-              strokeWidth="1"
-              fill="none"
-              fillRule="evenodd"
-            >
-              <g id="Desktop-HD" transform="translate(-24.000000, -550.000000)">
-                <g
-                  id="Source-FIles"
-                  transform="translate(24.000000, 549.000000)"
-                >
-                  <g id="Group" transform="translate(0.000000, 1.000000)">
-                    <g id="Studio-Logo">
-                      <rect
-                        id="Rectangle"
-                        fill="#FF4183"
-                        x="0"
-                        y="0"
-                        width="22"
-                        height="22"
-                      />
-                      <rect
-                        id="Rectangle"
-                        fill="#FFFFFF"
-                        x="12.1"
-                        y="12.1"
-                        width="5.5"
-                        height="5.5"
-                      />
-                    </g>
-                  </g>
-                </g>
-              </g>
-            </g>
-          </svg>
+          <StudioLogo />
           Studio
         </a>
         <a href="#">
