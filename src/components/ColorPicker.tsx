5f4f323368c90155053914e9714e3da140adcb55
Fang-Pen Lin <hello@fangpenlin.com>
2019-10-16 01:15:09 -0700

    Fix color alpha hex issue

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,13 +1,39 @@
 import "./ColorPicker.css";
 
+import { Color, SketchPicker } from "react-color";
 import React, { useState } from "react";
-
-import { SketchPicker } from "react-color";
 
 export interface Props {
   readonly color: string;
   readonly onChange: (color: string) => void;
 }
+
+const alphaToHex = (alpha: number) => {
+  if (alpha == 1) {
+    // just omit alpha hex part if it's 1
+    return "";
+  }
+  var hex: string = Math.trunc(alpha * 255).toString(16);
+  if (hex.length <= 1) {
+    hex = "0" + hex;
+  }
+  return hex;
+};
+
+const hexToRGBA = (hex: string): Color => {
+  if (!hex.startsWith("#")) {
+    return hex;
+  }
+  if (/^#([A-Fa-f0-9]{2}){4}$/.test(hex)) {
+    return {
+      r: parseInt(hex.substring(1, 3), 16),
+      g: parseInt(hex.substring(3, 5), 16),
+      b: parseInt(hex.substring(5, 7), 16),
+      a: parseInt(hex.substring(7, 9), 16) / 255
+    };
+  }
+  return hex;
+};
 
 const ColorPicker: React.FC<Props> = props => {
   const [active, setActive] = useState<boolean>(false);
@@ -34,8 +60,10 @@ const ColorPicker: React.FC<Props> = props => {
               }}
             ></div>
             <SketchPicker
-              color={color}
-              onChange={color => onChange(color.hex)}
+              color={hexToRGBA(color)}
+              onChange={color =>
+                onChange(color.hex + alphaToHex(color.rgb.a || 1))
+              }
             />
           </div>
         </div>
